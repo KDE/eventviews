9f4e1b985d436fa5dd0388ba13d3d04399fbd90e
Laurent Montel <montel@kde.org>
2021-09-08 13:53:07 +0200

    GIT_SILENT: use new akonadi includes

--- src/viewerapp/mainwindow.cpp
+++ src/viewerapp/mainwindow.cpp
@@ -21,8 +21,8 @@
 #include <Akonadi/Calendar/IncidenceChanger>
 #include <KCalendarCore/Event>
 
+#include <Akonadi/Collection>
 #include <Akonadi/ControlGui>
-#include <AkonadiCore/Collection>
 
 #include <KCheckableProxyModel>
 
