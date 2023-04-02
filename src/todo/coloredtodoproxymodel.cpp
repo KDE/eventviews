f0012247727e22263f10f32248a2571d5fbecd72
Kevin Ottens <ervin@kde.org>
2023-04-02 14:21:45 +0200

    Add a ColorTodoProxyModel providing the colored background feature
    This strongly assumes sitting on top of a TodoModel

---
+++ src/todo/coloredtodoproxymodel.cpp
@@ -0,0 +1,54 @@
+/*
+  SPDX-FileCopyrightText: 2023 Kevin Ottens <ervin@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later WITH Qt-Commercial-exception-1.0
+*/
+
+#include "coloredtodoproxymodel.h"
+
+#include "todomodel.h"
+
+class ColoredTodoProxyModelPrivate
+{
+public:
+    ColoredTodoProxyModelPrivate(const EventViews::PrefsPtr &preferences)
+        : m_preferences(preferences)
+    {
+    }
+
+    EventViews::PrefsPtr m_preferences;
+};
+
+static bool isDueToday(const KCalendarCore::Todo::Ptr &todo)
+{
+    return !todo->isCompleted() && todo->dtDue().date() == QDate::currentDate();
+}
+
+ColoredTodoProxyModel::ColoredTodoProxyModel(const EventViews::PrefsPtr &preferences, QObject *parent)
+    : QIdentityProxyModel(parent)
+    , d(new ColoredTodoProxyModelPrivate(preferences))
+{
+}
+
+ColoredTodoProxyModel::~ColoredTodoProxyModel() = default;
+
+QVariant ColoredTodoProxyModel::data(const QModelIndex &index, int role) const
+{
+    if (role == Qt::BackgroundRole) {
+        const auto todo
+            = QIdentityProxyModel::data(index, TodoModel::TodoPtrRole).value<KCalendarCore::Todo::Ptr>();
+        if (!todo) {
+            return {};
+        }
+
+        if (todo->isOverdue()) {
+            return QVariant(QBrush(d->m_preferences->todoOverdueColor()));
+        } else if (isDueToday(todo)) {
+            return QVariant(QBrush(d->m_preferences->todoDueTodayColor()));
+        } else {
+            return {};
+        }
+    }
+
+    return QIdentityProxyModel::data(index, role);
+}
---
+++ src/todo/coloredtodoproxymodel.h
@@ -0,0 +1,30 @@
+/*
+  SPDX-FileCopyrightText: 2023 Kevin Ottens <ervin@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later WITH Qt-Commercial-exception-1.0
+*/
+
+#pragma once
+
+#include "eventviews_export.h"
+#include "prefs.h"
+
+#include <QIdentityProxyModel>
+
+#include <memory>
+
+class ColoredTodoProxyModelPrivate;
+
+/**
+ * ProxyModel adding background color for overdue and due today items in a TodoModel
+ */
+class ColoredTodoProxyModel : public QIdentityProxyModel {
+public:
+    explicit ColoredTodoProxyModel(const EventViews::PrefsPtr &preferences, QObject *parent = nullptr);
+    ~ColoredTodoProxyModel() override;
+
+    Q_REQUIRED_RESULT QVariant data(const QModelIndex &index, int role) const override;
+
+private:
+    std::unique_ptr<ColoredTodoProxyModelPrivate> const d;
+};
