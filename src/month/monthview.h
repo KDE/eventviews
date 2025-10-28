8c80b8f887a504ff2703e05be461d35bd736ad38
Allen Winter <winter@kde.org>
2025-10-28 12:49:43 -0400

    Allow hiding the full window buttons in todo and month views.
    Part of the new feature for hiding the sidebar globally for all views.
    
    CCBUG: 511149

--- src/month/monthview.h
+++ src/month/monthview.h
@@ -59,6 +59,8 @@ public:
 
     [[nodiscard]] bool usesFullWindow();
 
+    void showFullWindowButton(bool show);
+
     [[nodiscard]] bool supportsDateRangeSelection() const
     {
         return false;
